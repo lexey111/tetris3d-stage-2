8ea288319574106e3fd56862ff2cafe94b14712e
Oleksii Koshkin <lexey111@gmail.com>
2023-06-20 17:41:58 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Tetris 3D Stage 2
+
+## Installation
+
+1. Clone repository (`git clone https://github.com/lexey111/tetris3d-stage-2.git` or other way).
+2. `npm install`
+3. `npm run dev`
+
+
+Read [Medium article]() for details.
---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import Next from "./components/Next.svelte";
+
+  const figures = "SZLJITO".split("");
+  let figure = "";
+
+  setInterval(() => {
+    figure = figures[Math.floor(Math.random() * figures.length)];
+  }, 500);
+</script>
+
+<main>
+  <Next {figure} />
+</main>
+
+<style>
+  /* nothing yet here */
+</style>
